417a61d9eb1c311ff02180752df8e7b77a59275d
varanitsa <varanitsa.es@gmail.com>
2023-01-15 19:08:21 +0100

    Initial commit

---
+++ src/test/java/com/springboot/app/rest_api/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
